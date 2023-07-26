d10387d7634c33de0f4c48dc502a7517e751ca33
咸小鱼 <1307083930@qq.com>
2023-07-26 23:14:47 +0800

    修改测试方法

--- bot/build.gradle.kts
+++ bot/build.gradle.kts
@@ -3,7 +3,7 @@ dependencies {
     implementation(fileTree("libs") { include("*.jar") })
     implementation("org.asynchttpclient:async-http-client:2.12.3")
     implementation("net.mamoe:mirai-core:2.15.0")
-    implementation("net.mamoe:mirai-core-mock:2.13.0")
+    testImplementation("net.mamoe:mirai-core-mock:2.13.0")
 }
 
 tasks.bootJar {
--- bot/src/test/kotlin/io/net/bot/BotApplicationTests.kt
+++ bot/src/test/kotlin/io/net/bot/BotApplicationTests.kt
@@ -1,13 +1,79 @@
 package io.net.bot
 
+import io.net.bot.core.BotRunner
+import io.net.bot.subscribe.GroupCmdSubscribe
+import kotlinx.coroutines.*
+import net.mamoe.mirai.LowLevelApi
+import net.mamoe.mirai.contact.MemberPermission
+import net.mamoe.mirai.event.globalEventChannel
+import net.mamoe.mirai.message.data.At
+import net.mamoe.mirai.message.data.PlainText
+import net.mamoe.mirai.mock.MockBot
+import net.mamoe.mirai.mock.MockBotFactory
+import net.mamoe.mirai.mock.utils.simpleMemberInfo
+import net.mamoe.mirai.utils.LoggerAdapters
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 
+@Suppress("UNUSED")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest
 class BotApplicationTests {
 
     @Test
-    fun contextLoads() {
+    @DisplayName("测试img")
+    fun img() {
+        mock("img")
     }
 
+    @set:Autowired
+    lateinit var groupCmdSubscribe: GroupCmdSubscribe
+
+    @MockBean
+    lateinit var botRunner: BotRunner
+
+    lateinit var bot: MockBot
+
+    val context = SupervisorJob() +
+            CoroutineName("MockBot-Coroutine") +
+            CoroutineExceptionHandler { _, e ->
+                e.printStackTrace()
+            }
+
+    @OptIn(LowLevelApi::class)
+    fun mock(msg: String) {
+        runBlocking {
+            val member = bot.addGroup(666, "testing!")
+                .addMember(simpleMemberInfo(5971, "test", permission = MemberPermission.OWNER))
+            member.says(At(bot) + PlainText(msg))
+        }
+    }
+
+    @BeforeAll
+    fun mockBot() {
+        LoggerAdapters.useLog4j2()
+        runBlocking {
+            CoroutineScope(context).launch(this.coroutineContext) {
+                MockBotFactory.newMockBotBuilder()
+                    .id(1307083930)
+                    .nick("augenstern-bot")
+                    .create()
+                    .run {
+                        this@BotApplicationTests.bot = this
+                        try {
+                            login()
+                        } catch (_: RuntimeException) {
+                            cancel()
+                            close()
+                        }
+                        globalEventChannel().registerListenerHost(groupCmdSubscribe.GroupListenerHost())
+                    }
+            }
+        }
+    }
 }
