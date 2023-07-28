ca8debe9e37ed448e6ea975ab6a0e353068ba5b2
咸小鱼 <1307083930@qq.com>
2023-07-28 22:06:58 +0800

    添加序列化接口

--- service-image/src/main/kotlin/io/net/image/bo/PixivRandomResultBO.kt
+++ service-image/src/main/kotlin/io/net/image/bo/PixivRandomResultBO.kt
@@ -1,10 +1,12 @@
 package io.net.image.bo
 
+import java.io.Serializable
+
 /**
  *@author Augenstern
  *@date 2023/6/4
  */
-data class PixivRandomResult(
+data class PixivRandomResultBO(
     val pid: Long = 0,
     val page: Int = 0,
     val uid: Long = 0,
@@ -15,4 +17,4 @@ data class PixivRandomResult(
     val height: Int = 0,
     val tags: List<String> = listOf(),
     val url: String = ""
-)
+) : Serializable
